d8d4967634c2e4b47056166407eacbe4f3c9d659
Admin <ankitpote1@gmail.com>
2026-02-11 23:41:40 +0530

    cloud trigger creted 4

--- src/main/java/com/example/testgcp/TestController.java
+++ src/main/java/com/example/testgcp/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String getHello(){
-        return "Please make it Possible GOd";
+        return "Hello World";
     }
 }
